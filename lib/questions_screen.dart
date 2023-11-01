0bb6c2fb2d74d80e5759b41a37e5c232c73778f7
MustafaGuer <mustafaguer@outlook.com>
2023-11-01 11:22:11 +0100

    add question change on answer tap

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -12,9 +12,18 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  var currentQuestionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      currentQuestionIndex++;
+    });
+  }
+
   @override
   Widget build(context) {
-    final currentQuestion = questions[0];
+    final currentQuestion = questions[currentQuestionIndex];
+
     return SizedBox(
       width: double.infinity,
       child: Container(
@@ -32,7 +41,8 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
               height: 30,
             ),
             ...currentQuestion.getShuffledAnswers().map(
-                  (answer) => AnswerButton(answerText: answer, onTap: () {}),
+                  (answer) =>
+                      AnswerButton(answerText: answer, onTap: answerQuestion),
                 ),
           ],
         ),
